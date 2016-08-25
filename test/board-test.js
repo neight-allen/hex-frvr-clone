a1f72b16eac4e7a6a5c10e7c1e71533ee1ebedcf
Nate Allen <neight.allen@gmail.com>
2016-08-25 10:04:11 -0600

    Added some board tests prior to refactor

--- test/board-test.js
+++ test/board-test.js
@@ -1,6 +1,9 @@
 const assert = require('chai').assert;
+var _ = require('underscore');
 
 const Board = require('../lib/board');
+const Shape = require('../lib/shape');
+const Tile = require('../lib/tile');
 
 describe('board', function(){
   context('initialization',function(){
@@ -30,7 +33,7 @@ describe('board', function(){
     })
   })
 
-  context('operations', function() {
+  context('slots', function() {
 
     it('should be able to get slot by coords', function() {
       var board = new Board();
@@ -48,6 +51,91 @@ describe('board', function(){
         assert.equal(slotFound.y, slot.y);
         assert.equal(slotFound.z, slot.z);
       })
+    });
+
+  });
+
+  context('shapes', function(){
+    it('can place shapes in the middle of a blank board', function(){
+      var board = new Board();
+      for(var i = 0; i < 50; i++){
+        assert(board.validShapeAtCoords(0,0,0, new Shape()));
+      }
+    })
+
+    it("can't place shapes on top of a tile", function(){
+        var board = new Board();
+        board.coordsToSlot(0,0,0).tile = new Tile();
+        for(var i = 0; i < 50; i++){
+          assert.isFalse(board.validShapeAtCoords(0,0,0, new Shape()));
+        }
+    });
+
+    it("has remaining moves when the board is empty", function(){
+      var board = new Board();
+      for(var i = 0; i < 50; i++){
+        assert(board.movesRemaining([new Shape(), new Shape(), new Shape()]));
+      }
+    });
+
+    it("has remaining moves when the board is empty", function(){
+      var board = new Board();
+      board.slots.forEach(function(slot){
+        slot.tile = new Tile();
+      })
+      for(var i = 0; i < 10; i++) {
+        assert.isFalse(board.movesRemaining([new Shape(), new Shape(), new Shape()]));
+      }
+    });
+
+  });
+
+  context("rows", function() {
+
+    it("can get a row of slots", function() {
+      var board = new Board();
+      _.range(-board.boardSize, board.boardSize).forEach(function(rowNum) {
+        ["x", "y", "z"].forEach(function(axis) {
+          board.getRow(axis, rowNum).forEach(function(slot) {
+            assert.equal(slot[axis], rowNum);
+          });
+        });
+      });
+    });
+
+    it("will remove a row that is full of tiles", function(){
+      var board = new Board();
+      _.range(-board.boardSize, board.boardSize).forEach(function(rowNum) {
+        ["x", "y", "z"].forEach(function(axis) {
+          var row = board.getRow(axis, rowNum)
+          row.forEach(function(slot) {
+            slot.tile = new Tile();
+          });
+          board.removeFullLines();
+          row.forEach(function(slot){
+            assert.isUndefined(slot.tile);
+          })
+        });
+      });
+    });
+
+    it("will remove multiple rows that are full of tiles", function(){
+      var board = new Board();
+      _.range(-board.boardSize, board.boardSize).forEach(function(rowNum) {
+        var rows = ["x", "y", "z"].map(function(axis) {
+          var row = board.getRow(axis, rowNum)
+          row.forEach(function(slot) {
+            slot.tile = new Tile();
+          });
+          return row
+        });
+        board.removeFullLines();
+        rows.forEach(function(row) {
+          row.forEach(function(slot) {
+            assert.isUndefined(slot.tile);
+          })
+        })
+      });
     });
 
   });
